72e882a2431043185892f303b3f74bc0856625f8
nwatson <nwatson@ctr.lanterds.com>
2024-12-30 14:46:06 -0600

    Implement changes in https://github.com/dbt-labs/dbt-external-tables/pull/236

--- macros/plugins/databricks/helpers/recover_partitions.sql
+++ macros/plugins/databricks/helpers/recover_partitions.sql
@@ -1,4 +1,4 @@
-{% macro spark__recover_partitions(source_node) %}
+{% macro databricks__recover_partitions(source_node) %}
     {# https://docs.databricks.com/sql/language-manual/sql-ref-syntax-ddl-alter-table.html #}
 
     {%- if source_node.external.partitions and source_node.external.using and source_node.external.using|lower != 'delta' -%}
@@ -11,8 +11,4 @@
 
     {{return(ddl)}}
 
-{% endmacro %}
-
-{% macro recover_partitions(source_node) %}
-    {{ return(adapter.dispatch('recover_partitions', 'dbt_external_tables')(source_node)) }}
 {% endmacro %}
